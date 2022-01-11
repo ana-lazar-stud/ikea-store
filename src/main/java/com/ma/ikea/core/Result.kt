d52c23b0c3676d593059376c13b74d33c269ae22
ana-lazar <54860891+ana-lazar@users.noreply.github.com>
2022-01-11 13:42:14 +0200

    added server

--- src/main/java/com/ma/ikea/core/Result.kt
+++
@@ -1,8 +0,0 @@
-package com.ma.ikea.core
-
-sealed class Result<out R> {
-
-    data class Success<out T>(val data: T) : Result<T>()
-
-    data class Error(val exception: Exception) : Result<Nothing>()
-}
